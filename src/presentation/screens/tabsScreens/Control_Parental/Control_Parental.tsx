501c7a0f7dd6a41f3f04ab3528ced666e5200474
ArisEspino <espinozaariss20@gmail.com>
2024-10-16 19:52:56 -0600

    Primer commit

--- src/presentation/screens/tabsScreens/Control_Parental/Control_Parental.tsx
+++ src/presentation/screens/tabsScreens/Control_Parental/Control_Parental.tsx
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'flex-start', // arriba
         paddingTop: 160, // more space
-        backgroundColor: '#fff',
+        backgroundColor: 'red',
     },
 
 
